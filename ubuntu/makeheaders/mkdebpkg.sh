f4a26f5b250784604c5c1666f981a5a1e3080a6e
Kojiro ONO <ono.kojiro@gmail.com>
2021-07-31 09:12:59 +0900

    add mkdebpkg for makeheaders

---
+++ ubuntu/makeheaders/mkdebpkg.sh
@@ -0,0 +1,140 @@
+#!/bin/sh
+
+top_dir="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+cd $top_dir
+
+REALNAME=fossil
+PKGNAME=makeheaders
+VERSION=2.16
+ARCHIVE=fossil-src-2.16.tar.gz
+URL=https://fossil-scm.org/home/tarball/7aedd5675883d4412cf20917d340b6985e3ecb842e88a39f135df034b2d5f4d3/fossil-src-2.16.tar.gz
+
+SHA256SUM=fab37e8093932b06b586e99a792bf9b20d00d530764b5bddb1d9a63c8cdafa14
+
+DESTDIR=~/tmp/$PKGNAME-$VERSION
+OUTPUTDIR=.
+
+all()
+{
+  fetch 
+  extract
+  #configure
+  build
+  install
+  #custom_install
+  package
+  #clean
+}
+
+help()
+{
+  echo "$0 <subcommand>"
+  echo "  subcommand :"
+  echo "     fetch"
+  echo "     extract"
+  echo "     configure"
+  echo "     build"
+  echo "     install"
+  echo "     custom_install"
+  echo "     package"
+  echo "     clean"
+}
+
+
+fetch()
+{
+  if [ ! -e ${ARCHIVE} ]; then
+    curl -o ${ARCHIVE} ${URL}
+  else
+    echo "skip download"
+  fi
+}
+
+check()
+{
+  echo "$SHA256SUM  ${ARCHIVE}" | sha256sum -c -
+}
+
+
+extract()
+{
+	if [ ! -e $REALNAME-src-$VERSION ]; then
+		tar xzvf $ARCHIVE;
+	fi
+}
+
+configure()
+{
+	cd $REALNAME-$VERSION
+	sh configure \
+	  --prefix=/usr \
+	  --enable-interwork \
+	  --enable-multilib \
+	  --enable-plugins \
+	  --disable-nls \
+	  --disable-shared \
+	  --disable-threads \
+	  --with-gcc --with-gnu-as --with-gnu-ld \
+	  --with-docdir=share/doc/$PKGNAME \
+	  --disable-werror
+
+	cd ..
+}
+
+build()
+{
+  echo build
+  cd $REALNAME-src-$VERSION
+  gcc -o makeheaders src/makeheaders.c
+  cd ..
+}
+
+install()
+{
+  echo install
+  mkdir -p $DESTDIR/usr/bin
+  cd $REALNAME-src-$VERSION
+  cp -a makeheaders $DESTDIR/usr/bin/
+  cd ..
+}
+
+custom_install()
+{
+  cd $REALNAME-$VERSION
+
+  cd $top_dir
+}
+
+package()
+{
+	mkdir -p $DESTDIR/DEBIAN
+cat << EOS > $DESTDIR/DEBIAN/control
+Package: $PKGNAME
+Maintainer: Kojiro ONO <ono.kojiro@gmail.com>
+Architecture: amd64
+Version: $VERSION
+Description: $PKGNAME
+EOS
+	fakeroot dpkg-deb --build $DESTDIR $OUTPUTDIR
+}
+
+clean()
+{
+  rm -rf $REALNAME-src-$VERSION
+}
+
+
+if [ "$#" = 0 ]; then
+  all
+fi
+
+for target in "$@"; do
+	type $target | grep function
+	res=$?
+	if [ "x$res" = "x0" ]; then
+		$target
+	else
+		echo invalid target, "$target"
+	fi
+done
+
