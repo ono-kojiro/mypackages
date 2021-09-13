56df7c858f43b17f6a3e2daa1305fd7d95cba3cd
Kojiro ONO <ono.kojiro@gmail.com>
2021-09-13 21:32:22 +0900

    support of 3.9

--- yocto/rocko/iperf3/mkpackage.sh
+++ yocto/rocko/iperf3/mkpackage.sh
@@ -2,10 +2,21 @@
 
 top_dir="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
 
+pkgname=iperf
+pkgver=3.9
+archive=${pkgname}-${pkgver}.tar.gz
+src_url=https://downloads.es.net/pub/iperf/$archive
+
+if [ ! -e ./rpmbuild/SOURCES/$archive ]; then
+  mkdir -p ./rpmbuild/SOURCES/
+  wget $src_url
+  mv -f $archive ./rpmbuild/SOURCES/
+fi
+
 rpmbuild -bb \
   --nodeps \
   --target=aarch64-poky-linux \
   --define="_topdir $top_dir/rpmbuild" \
   --define="_build x86_64-linux-gnu" \
-  iperf-3.7.spec
+  iperf-${pkgver}.spec
 
