aec82da872b6c1c1e43027c4db93c5969c6544e2
Kojiro ONO <ono.kojiro@gmail.com>
2021-09-13 21:23:59 +0900

    change top_dir

--- yocto/rocko/iperf3/mkpackage.sh
+++ yocto/rocko/iperf3/mkpackage.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+top_dir="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+
 rpmbuild -bb \
   --nodeps \
   --target=aarch64-poky-linux \
+  --define="_topdir $top_dir/rpmbuild" \
   --define="_build x86_64-linux-gnu" \
   iperf-3.7.spec
 
