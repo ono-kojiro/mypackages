7aa48109e074ebb452be6752ef2ca66357de203d
Kojiro ONO <ono.kojiro@gmail.com>
2021-09-15 08:29:32 +0900

    add sysbench.spec

--- yocto/rocko/sysbench/mkpackage.sh
+++ yocto/rocko/sysbench/mkpackage.sh
@@ -2,17 +2,26 @@
 
 top_dir="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
 
+which aarch64-poky-linux-gcc
+res=$?
+if [ "$res" != "0" ]; then
+  echo "ERROR : aarch64-poky-linux-gcc NOT found."
+  exit 1
+fi
+
 pkgname=sysbench
-pkgver=0.4.12.10
-archive=${pkgname}-${pkgver}.tar.gz
+pkgver=0.4.12
+archive=${pkgname}-${pkgver}.10.tar.gz
 #src_url=https://github.com/akopytov/sysbench/archive/refs/tags/${archive}
-src_url=http://downloads.mysql.com/source/sysbench-0.4.12.10.tar.gz
+src_url=http://downloads.mysql.com/source/${archive}
 
 if [ ! -e ./rpmbuild/SOURCES/${pkgname}-${pkgver}.tar.gz ]; then
   mkdir -p ./rpmbuild/SOURCES/
   wget $src_url
   mv -f $archive ./rpmbuild/SOURCES/${pkgname}-${pkgver}.tar.gz
 fi
+
+cp -f 0000-disable_ac_lib_prefix.patch ./rpmbuild/SOURCES/
 
 rpmbuild -bb \
   --nodeps \
