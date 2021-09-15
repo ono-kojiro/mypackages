ea97e741fc36f19c138d2452408c2b7f4692828d
Kojiro ONO <ono.kojiro@gmail.com>
2021-09-15 21:59:06 +0900

    add nano for yocto/rocko

---
+++ yocto/rocko/nano/mkpackage.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+top_dir="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+
+which aarch64-poky-linux-gcc > /dev/null 2>&1
+res=$?
+if [ "$res" != "0" ]; then
+  echo "ERROR : aarch64-poky-linux-gcc NOT found."
+  exit 1
+fi
+
+pkgname=nano
+pkgver=5.8
+archive=${pkgname}-${pkgver}.tar.xz
+src_url=https://www.nano-editor.org/dist/v5/${pkgname}-${pkgver}.tar.xz
+specfile=nano.spec
+  
+spectool -g -C $top_dir/rpmbuild/SOURCES ${specfile}
+
+cp -f nano-5.8-die-infinite-recursion.patch ./rpmbuild/SOURCES/
+cp -f nano-default-editor.* ./rpmbuild/SOURCES/
+cp -f nanorc ./rpmbuild/SOURCES/
+
+rpmbuild -bb \
+  --nodeps \
+  --target=aarch64-poky-linux \
+  --define="version $pkgver" \
+  --define="_topdir $top_dir/rpmbuild" \
+  --define="_build x86_64-linux-gnu" \
+  --define="_lib lib" \
+  --define="_libdir /usr/lib" \
+  ${specfile}
+
