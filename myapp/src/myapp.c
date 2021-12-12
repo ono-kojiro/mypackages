fad836d7fbfb7d830304c1a1b69fe1f50ef7b3c6
Kojiro ONO <ono.kojiro@gmail.com>
2021-12-12 16:38:08 +0900

    use AC_ARG_WITH for mylib

--- myapp/src/myapp.c
+++ myapp/src/myapp.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
+#ifdef HAVE_LIBMYLIB
 #include <mylib.h>
+#endif
 
 int main(int argc, char **argv)
 {
 	printf("This is myapp.\n");
+#ifdef HAVE_LIBMYLIB
 	mylib();
+#else
+	printf("mylib() is not called.\n"); 
+#endif
 	return 0;
 }
 
