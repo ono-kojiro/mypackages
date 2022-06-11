9ea662f8521a87a0ad6d78579db5c30535749581
Kojiro ONO <ono.kojiro@gmail.com>
2022-06-11 16:09:45 +0900

    add build script for erlang

---
+++ ubuntu/erlang/build.sh
@@ -0,0 +1,186 @@
+#!/bin/sh
+
+set -e
+
+top_dir="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+cd $top_dir
+
+realname="otp"
+pkgname="${realname}"
+version="25.0.1"
+
+src_urls=""
+src_urls="$src_urls https://github.com/erlang/otp/releases/download/OTP-25.0.1/otp_src_25.0.1.tar.gz"
+
+url="https://www.erlang.org/"
+
+sourcedir=$top_dir/work/sources
+builddir=$top_dir/work/build
+destdir=$top_dir/work/dest/${pkgname}-${version}
+
+outputdir=$top_dir
+
+all()
+{
+  fetch
+  extract
+  configure
+  compile
+  install
+  custom_install
+  package
+}
+
+fetch()
+{
+  mkdir -p $sourcedir
+
+  for src_url in $src_urls; do
+    archive=`basename $src_url`
+    case $src_url in
+      *.gz | *.zip )
+        if [ ! -e "$sourcedir/$archive" ]; then
+            wget $src_url
+            mv -f $archive $sourcedir/
+        else
+            echo "skip $src_url"
+        fi
+        ;;
+      *.git )
+        dirname=${archive%.git}
+        if [ ! -d "${sourcedir}/${dirname}" ]; then
+            mkdir -p ${sourcedir}
+            git -C ${sourcedir} clone $src_url
+        else
+            echo "skip git-clone"
+        fi
+        ;;
+      * )
+        echo "ERROR : unknown extension, $src_url"
+        exit 1
+        ;;
+    esac
+  done
+
+}
+
+extract()
+{
+  mkdir -p ${builddir}
+  
+  for src_url in $src_urls; do
+    archive=`basename $src_url`
+    basename=`basename $src_url .tar.gz`
+    case $src_url in
+      *.gz )
+        if [ ! -d "${builddir}/${basename}" ]; then
+          tar -C ${builddir} -xvf ${sourcedir}/${archive}
+        else
+          echo "skip $archive"
+        fi
+        ;;
+      *.zip )
+        if [ ! -d "${builddir}/${basename}" ]; then
+          unzip ${sourcedir}/${archive} -d ${builddir}
+        else
+          echo "skip $archive"
+        fi
+        ;;
+      *.git )
+        dirname=${archive%.git}
+        if [ ! -d "${builddir}/${dirname}" ]; then
+            mkdir -p ${builddir}
+            cp -a ${sourcedir}/${dirname} ${builddir}/
+        else
+            echo "skip extract"
+        fi
+        ;;
+      * )
+        echo "ERROR : unknown extension, $src_url"
+        exit 1
+        ;;
+    esac
+  done
+
+}
+
+configure()
+{
+  cd ${builddir}/${pkgname}_src_${version}
+  ./configure --prefix=/usr
+  cd ${top_dir}
+}
+
+config()
+{
+  configure
+}
+
+compile()
+{
+  cd ${builddir}/${pkgname}_src_${version}
+  make -j7
+  cd ${top_dir}
+}
+
+run()
+{
+  qemu-system-aarch64 \
+    -M arm-generic-fdt \
+    -nographic \
+    -serial mon:stdio \
+    -dtb /usr/share/qemu/xilinx/SINGLE_ARCH/zcu102-arm.dtb \
+    -device loader,file=${builddir}/${pkgname}_${version}/obj/${pkgname},cpu-num=4 \
+    -device loader,addr=0xff5e023c,data=0x80008fde,data-len=4 \
+    -device loader,addr=0xff9a0000,data=0x80000218,data-len=4
+}
+
+install()
+{
+  cd ${builddir}/${pkgname}_src_${version}
+  rm -rf ${destdir}
+  make install DESTDIR=${destdir}
+  cd ${top_dir}
+}
+
+custom_install()
+{
+  :
+}
+
+package()
+{
+	mkdir -p $destdir/DEBIAN
+
+    username=`git config user.name`
+    email=`git config user.email`
+
+cat << EOS > $destdir/DEBIAN/control
+Package: $pkgname
+Maintainer: $username <$email>
+Architecture: amd64
+Version: $version
+Description: $pkgname
+EOS
+	fakeroot dpkg-deb --build $destdir $outputdir
+}
+
+clean()
+{
+  rm -rf $builddir
+  rm -rf $destdir
+}
+
+if [ "$#" = 0 ]; then
+  all
+fi
+
+for target in "$@"; do
+	num=`LANG=C type $target | grep 'function' | wc -l`
+	if [ $num -ne 0 ]; then
+		$target
+	else
+		echo invalid target, "$target"
+	fi
+done
+
