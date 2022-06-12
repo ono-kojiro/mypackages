24d2aa7f6fa0b137bf40f66aaf3a02c4963dae3f
Kojiro ONO <ono.kojiro@gmail.com>
2022-06-12 02:09:32 +0000

    add build.sh for rebar3

---
+++ ubuntu/rebar3/build.sh
@@ -0,0 +1,185 @@
+#!/bin/sh
+
+set -e
+
+top_dir="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+cd $top_dir
+
+realname="rebar3"
+pkgname="${realname}"
+version="3.18.0"
+
+src_urls=""
+src_urls="$src_urls https://github.com/erlang/rebar3/archive/refs/tags/3.18.0.tar.gz"
+
+url="https://github.com/erlang/rebar3"
+
+sourcedir=$top_dir/work/sources
+builddir=$top_dir/work/build
+destdir=$top_dir/work/dest/${pkgname}-${version}
+
+outputdir=$top_dir
+
+all()
+{
+  fetch
+  extract
+  configure
+  compile
+  install
+  custom_install
+  package
+}
+
+fetch()
+{
+  mkdir -p $sourcedir
+
+  for src_url in $src_urls; do
+    archive=`basename $src_url`
+    case $src_url in
+      *.gz | *.zip )
+        if [ ! -e "$sourcedir/$archive" ]; then
+            wget $src_url
+            mv -f $archive $sourcedir/
+        else
+            echo "skip $src_url"
+        fi
+        ;;
+      *.git )
+        dirname=${archive%.git}
+        if [ ! -d "${sourcedir}/${dirname}" ]; then
+            mkdir -p ${sourcedir}
+            git -C ${sourcedir} clone $src_url
+        else
+            echo "skip git-clone"
+        fi
+        ;;
+      * )
+        echo "ERROR : unknown extension, $src_url"
+        exit 1
+        ;;
+    esac
+  done
+
+}
+
+extract()
+{
+  mkdir -p ${builddir}
+  
+  for src_url in $src_urls; do
+    archive=`basename $src_url`
+    basename=`basename $src_url .tar.gz`
+    case $src_url in
+      *.gz )
+        if [ ! -d "${builddir}/${basename}" ]; then
+          tar -C ${builddir} -xvf ${sourcedir}/${archive}
+        else
+          echo "skip $archive"
+        fi
+        ;;
+      *.zip )
+        if [ ! -d "${builddir}/${basename}" ]; then
+          unzip ${sourcedir}/${archive} -d ${builddir}
+        else
+          echo "skip $archive"
+        fi
+        ;;
+      *.git )
+        dirname=${archive%.git}
+        if [ ! -d "${builddir}/${dirname}" ]; then
+            mkdir -p ${builddir}
+            cp -a ${sourcedir}/${dirname} ${builddir}/
+        else
+            echo "skip extract"
+        fi
+        ;;
+      * )
+        echo "ERROR : unknown extension, $src_url"
+        exit 1
+        ;;
+    esac
+  done
+
+}
+
+prepare()
+{
+  sudo apt -y install libncurses-dev
+}
+
+configure()
+{
+  cd ${builddir}/${pkgname}-${version}
+  #./configure --prefix=/usr
+  ./bootstrap
+  cd ${top_dir}
+}
+
+config()
+{
+  configure
+}
+
+compile()
+{
+  cd ${builddir}/${pkgname}-${version}
+  make -j7
+  cd ${top_dir}
+}
+
+install()
+{
+  cd ${builddir}/${pkgname}-${version}
+  rm -rf ${destdir}
+  #make install DESTDIR=${destdir}
+  ./rebar3 local install
+
+  mkdir -p ${destdir}/usr
+  cp -a $HOME/.cache/rebar3/* ${destdir}/usr/
+  rm -rf ${destdir}/usr/vsns/
+  cd ${top_dir}
+}
+
+custom_install()
+{
+  :
+}
+
+package()
+{
+  mkdir -p $destdir/DEBIAN
+
+  username=`git config user.name`
+  email=`git config user.email`
+
+cat << EOS > $destdir/DEBIAN/control
+Package: $pkgname
+Maintainer: $username <$email>
+Architecture: amd64
+Version: $version
+Description: $pkgname
+EOS
+	fakeroot dpkg-deb --build $destdir $outputdir
+}
+
+clean()
+{
+  rm -rf $builddir
+  rm -rf $destdir
+}
+
+if [ "$#" = 0 ]; then
+  all
+fi
+
+for target in "$@"; do
+	num=`LANG=C type $target | grep 'function' | wc -l`
+	if [ $num -ne 0 ]; then
+		$target
+	else
+		echo invalid target, "$target"
+	fi
+done
+
