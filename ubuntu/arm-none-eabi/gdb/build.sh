d194ffbf3ac5aca3b755fcd95718d4bf228802c9
Kojiro ONO <ono.kojiro@gmail.com>
2022-07-30 05:00:48 +0900

    add arm-none-eabi example

---
+++ ubuntu/arm-none-eabi/gdb/build.sh
@@ -0,0 +1,205 @@
+#!/bin/sh
+
+set -e
+
+top_dir="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+cd $top_dir
+
+target="arm-none-eabi"
+realname="gdb"
+pkgname="${realname}-${target}"
+version="11.2"
+#version="12.1"
+
+src_urls=""
+src_urls="$src_urls https://ftp.jaist.ac.jp/pub/GNU/gdb/gdb-${version}.tar.xz"
+
+url="https://sourceware.org/gdb/"
+
+sourcedir=$top_dir/work/sources
+builddir=$top_dir/work/build
+destdir=$top_dir/work/dest/${pkgname}-${version}
+
+outputdir=$top_dir
+
+all()
+{
+  fetch
+  extract
+  configure
+  compile
+  install
+  custom_install
+  package
+}
+
+fetch()
+{
+  mkdir -p $sourcedir
+
+  for src_url in $src_urls; do
+    archive=`basename $src_url`
+    case $src_url in
+      *.gz | *.xz | *.zip )
+        if [ ! -e "$sourcedir/$archive" ]; then
+            wget $src_url
+            mv -f $archive $sourcedir/
+        else
+            echo "skip $src_url"
+        fi
+        ;;
+      *.git )
+        dirname=${archive%.git}
+        if [ ! -d "${sourcedir}/${dirname}" ]; then
+            mkdir -p ${sourcedir}
+            git -C ${sourcedir} clone $src_url
+        else
+            echo "skip git-clone"
+        fi
+        ;;
+      * )
+        echo "ERROR : unknown extension, $src_url"
+        exit 1
+        ;;
+    esac
+  done
+
+}
+
+extract()
+{
+  mkdir -p ${builddir}
+  
+  for src_url in $src_urls; do
+    archive=`basename $src_url`
+    basename=`basename $src_url .tar.gz`
+    case $src_url in
+      *.gz | *.xz )
+        if [ ! -d "${builddir}/${basename}" ]; then
+          tar -C ${builddir} -xvf ${sourcedir}/${archive}
+        else
+          echo "skip $archive"
+        fi
+        ;;
+      *.zip )
+        if [ ! -d "${builddir}/${basename}" ]; then
+          unzip ${sourcedir}/${archive} -d ${builddir}
+        else
+          echo "skip $archive"
+        fi
+        ;;
+      *.git )
+        dirname=${archive%.git}
+        if [ ! -d "${builddir}/${dirname}" ]; then
+            mkdir -p ${builddir}
+            cp -a ${sourcedir}/${dirname} ${builddir}/
+        else
+            echo "skip extract"
+        fi
+        ;;
+      * )
+        echo "ERROR : unknown extension, $src_url"
+        exit 1
+        ;;
+    esac
+  done
+
+}
+
+prepare()
+{
+  sudo apt -y install libexpat1-dev libpython3-dev
+}
+
+configure()
+{
+  cd ${builddir}/${realname}-${version}
+  rm -rf build
+  mkdir -p build
+  cd build
+  sh ../configure \
+    --prefix=/usr \
+    --target=$target \
+    --enable-sim
+  cd ${top_dir}
+}
+
+config()
+{
+  configure
+}
+
+compile()
+{
+  cd ${builddir}/${realname}-${version}
+  cd build
+  make -j7
+  cd ${top_dir}
+}
+
+build()
+{
+  compile
+}
+
+install()
+{
+  cd ${builddir}/${realname}-${version}
+  cd build
+  rm -rf ${destdir}
+  make install DESTDIR=${destdir}
+
+  rm -f ${destdir}/usr/include/gdb/jit-reader.h
+  rm -rf ${destdir}/usr/share/gdb
+  rm -rf ${destdir}/usr/share/info
+  rm -rf ${destdir}/usr/share/locale
+  cd ${top_dir}
+}
+
+custom_install()
+{
+  :
+}
+
+package()
+{
+  mkdir -p $destdir/DEBIAN
+
+  username=`git config user.name`
+  email=`git config user.email`
+
+cat << EOS > $destdir/DEBIAN/control
+Package: $pkgname
+Maintainer: $username <$email>
+Architecture: amd64
+Version: $version
+Description: $pkgname
+EOS
+  
+  fakeroot dpkg-deb --build $destdir $outputdir
+}
+
+sysinstall()
+{
+  sudo apt -y install ./${pkgname}_${version}_amd64.deb
+}
+
+clean()
+{
+  rm -rf $builddir
+  rm -rf $destdir
+}
+
+if [ "$#" = 0 ]; then
+  all
+fi
+
+for target in "$@"; do
+	num=`LANG=C type $target | grep 'function' | wc -l`
+	if [ $num -ne 0 ]; then
+		$target
+	else
+		echo invalid target, "$target"
+	fi
+done
+
