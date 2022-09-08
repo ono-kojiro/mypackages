acc45fa0cecb582eda19f3b27fe52f029d703a99
Kojiro ONO <ono.kojiro@gmail.com>
2022-09-08 13:42:11 +0000

    add package list

---
+++ ubuntu/bionic/install_packaged.sh
@@ -0,0 +1,41 @@
+ #!/bin/sh
+
+sudo apt -y install \ 
+    autoconf \
+    automake \
+    autopoint \
+    build-essential \
+    check \
+    cmake \
+    gettext \
+    libarchive-dev \
+    libbz2-dev \
+    libcairo2-dev
+    libcppunit-dev \
+    libcurl4-gnutls-dev \
+    libcurl4-openssl-dev \
+    libgcrypt20-dev \
+    libgirepository1.0-dev \
+    libglib2.0-dev \
+    libgpgme-dev \
+    libjson-c-dev \
+    liblua5.3-dev \
+    liblzma-dev \
+    libmagic-dev \
+    libncurses-dev \
+    libpopt-dev \
+    libpython3-dev
+    librpm-dev
+    libsmartcols-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    libtool \
+    libxml2-dev \
+    libyaml-dev \
+    libzstd-dev \
+    pkg-config \
+    python3-distutils \
+    python3-gpg \
+    swig \
+    zlib1g-dev
+
