2c5f19220e87f64f5f2ba33df73db8790d7b5800
Kojiro ONO <ono.kojiro@gmail.com>
2022-12-16 22:32:06 +0900

    add build script for bc

---
+++ ubuntu/bc/build.sh
@@ -0,0 +1,207 @@
+#!/bin/sh
+
+set -e
+
+top_dir="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+cd $top_dir
+
+realname="bc"
+pkgname="${realname}"
+pkgver="1.07.1"
+
+src_urls=""
+src_urls="$src_urls https://ftp.jaist.ac.jp/pub/GNU/bc/bc-1.07.1.tar.gz"
+
+url="https://www.gnu.org/software/bc/"
+
+sourcedir=$top_dir/work/sources
+builddir=$top_dir/work/build
+destdir=$top_dir/work/dest/${pkgname}-${pkgver}
+
+outputdir=$top_dir
+
+all()
+{
+  fetch
+  extract
+  patch
+  configure
+  compile
+  install
+  custom_install
+  package
+}
+
+fetch()
+{
+  mkdir -p $sourcedir
+
+  for src_url in $src_urls; do
+    archive=`basename $src_url`
+    case $src_url in
+      *.gz | *.zip )
+        if [ ! -e "$sourcedir/$archive" ]; then
+            wget $src_url
+            mv -f $archive $sourcedir/
+        else
+            echo "skip $src_url"
+        fi
+        ;;
+      *.git )
+        dirname=${archive%.git}
+        if [ ! -d "${sourcedir}/${dirname}" ]; then
+            mkdir -p ${sourcedir}
+            git -C ${sourcedir} clone $src_url
+        else
+            echo "skip git-clone"
+        fi
+        ;;
+      * )
+        echo "ERROR : unknown extension, $src_url"
+        exit 1
+        ;;
+    esac
+  done
+
+}
+
+extract()
+{
+  mkdir -p ${builddir}
+  
+  for src_url in $src_urls; do
+    archive=`basename $src_url`
+    basename=`basename $src_url .tar.gz`
+    case $src_url in
+      *.gz )
+        if [ ! -d "${builddir}/${basename}" ]; then
+          tar -C ${builddir} -xvf ${sourcedir}/${archive}
+        else
+          echo "skip $archive"
+        fi
+        ;;
+      *.zip )
+        if [ ! -d "${builddir}/${basename}" ]; then
+          unzip ${sourcedir}/${archive} -d ${builddir}
+        else
+          echo "skip $archive"
+        fi
+        ;;
+      *.git )
+        dirname=${archive%.git}
+        if [ ! -d "${builddir}/${dirname}" ]; then
+            mkdir -p ${builddir}
+            cp -a ${sourcedir}/${dirname} ${builddir}/
+        else
+            echo "skip extract"
+        fi
+        ;;
+      * )
+        echo "ERROR : unknown extension, $src_url"
+        exit 1
+        ;;
+    esac
+  done
+
+}
+
+prepare()
+{
+  sudo apt -y install \
+    python3-gpg
+}
+
+patch()
+{
+  cd ${builddir}/${pkgname}-${pkgver}
+  #command patch -p0 -i ${top_dir}/0000-change_install_dir.patch
+  cd ${top_dir}
+}
+
+configure()
+{
+  cd ${builddir}/${pkgname}-${pkgver}
+  mkdir -p build
+  cd build
+  sh ../configure --prefix=/usr
+  cd ${top_dir}
+}
+
+config()
+{
+  configure
+}
+
+compile()
+{
+  cd ${builddir}/${pkgname}-${pkgver}
+  cd build
+  make clean
+  make
+  cd ${top_dir}
+}
+
+build()
+{
+  compile
+}
+
+install()
+{
+  cd ${builddir}/${pkgname}-${pkgver}
+  cd build
+  rm -rf ${destdir}
+  make install DESTDIR=${destdir}
+  rm -rf ${destdir}/usr/share/info/dir
+  cd ${top_dir}
+
+  custom_install
+}
+
+custom_install()
+{
+  cd ${destdir}/usr/bin
+  cd ${top_dir}
+}
+
+package()
+{
+  mkdir -p $destdir/DEBIAN
+
+  username=`git config user.name`
+  email=`git config user.email`
+
+cat << EOS > $destdir/DEBIAN/control
+Package: $pkgname
+Maintainer: $username <$email>
+Architecture: amd64
+Version: $pkgver
+Description: $pkgname
+EOS
+	fakeroot dpkg-deb --build $destdir $outputdir
+}
+
+clean()
+{
+  rm -rf $builddir
+  rm -rf $destdir
+}
+
+sysinstall()
+{
+  sudo apt -y install ./${pkgname}_${pkgver}_amd64.deb
+}
+
+if [ "$#" = 0 ]; then
+  all
+fi
+
+for target in "$@"; do
+	num=`LANG=C type $target | grep 'function' | wc -l`
+	if [ $num -ne 0 ]; then
+		$target
+	else
+		echo invalid target, "$target"
+	fi
+done
+
