eb7458fc6007903ea2e1abc73a8f25ac470a6a32
Kojiro ONO <ono.kojiro@gmail.com>
2026-07-27 22:39:16 +0900

    support TLS (but not stable now)

--- ubuntu/seaweedfs/build.sh
+++ ubuntu/seaweedfs/build.sh
@@ -186,10 +186,14 @@ install()
 
   mkdir -p etc/seaweedfs/
   command install -m 644 ${top_dir}/filer.toml etc/seaweedfs/
+  command install -m 644 ${top_dir}/security.toml etc/seaweedfs/
 
   for component in ${components}; do
     command install -m 644 ${top_dir}/${component}.env etc/seaweedfs/
   done
+    
+  command install -m 644 ${top_dir}/tls.env etc/seaweedfs/
+
   cd ${top_dir}
 }
 
